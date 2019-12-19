5337956fc385cfae99a20e47919ff7ddbf125617
John <JohnPhamDeveloper@hotmail.com>
2019-12-19 01:02:06 -0800

    project init

---
+++ memorygame/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Login = () => {
+  return (
+    <div className="login__page">
+      <form className="login__form">
+        <label></label>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
