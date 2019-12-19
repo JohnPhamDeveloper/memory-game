e5e9734a4ee74b7b4deee1fb301af88d163a644e
John <JohnPhamDeveloper@hotmail.com>
2019-12-19 15:35:59 -0800

    Cards shuffled

--- memorygame/src/components/Cards.js
+++ memorygame/src/components/Cards.js
@@ -24,6 +24,8 @@ const Cards = ({ numberOfCards }) => {
       tempCards.push(component, component2); // Push twice so that there are two cards that can match
     }
 
+    shuffle(tempCards);
+
     setCards(tempCards);
   };
 
@@ -31,6 +33,16 @@ const Cards = ({ numberOfCards }) => {
 
   const renderCards = () => {
     return cards.map(card => card);
+  };
+
+  // https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+  const shuffle = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
   };
 
   return <div className="cards">{renderCards()}</div>;
