3772ccb4d245ef428bec41bc6d3781b416b6b925
John <JohnPhamDeveloper@hotmail.com>
2019-12-19 01:44:40 -0800

    adding SASS

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import Login from './pages/Login';
+import './app.scss';
+import './typography.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- memorygame/src/pages/Login.js
+++ memorygame/src/pages/Login.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import './Login.scss';
 
 const Login = () => {
   return (
-    <div className="login__page">
-      <form className="login__form">
-        <label></label>
+    <div className="login-page">
+      {/* LOGIN FORM */}
+      <form className="login-form">
+        {/* USERNAME */}
+        <label className="login-form__username-label" htmlFor="username">
+          Username:
+          <input type="text" className="login-form__username-input" id="username" name="username" />
+        </label>
+        {/* PASSWORD */}
+        <label className="login-form__password-label" htmlFor="password">
+          Password:
+          <input
+            className="login-form__password-input"
+            type="password"
+            id="password"
+            name="password"
+          />
+        </label>
       </form>
     </div>
   );
